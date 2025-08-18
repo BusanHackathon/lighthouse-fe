e5ead4a11edda4edc90a3454af944c39e8a35987
Dobbymin <gmin990624@gmail.com>
2025-08-18 19:58:54 +0900

    feat: 라우터에 로그인, 회원가입, 위험도 및 신뢰도 진단 페이지 추가

--- src/app/routes/Router.tsx
+++ src/app/routes/Router.tsx
@@ -1,12 +1,34 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import { MainPage } from '@/pages';
+import {
+  LoginPage,
+  MainPage,
+  ReliabilityDiagnosticsPage,
+  RiskDiagnosticsPage,
+  SignupPage,
+} from '@/pages';
 import { ROUTER_PATH } from '@/shared';
 
 const router = createBrowserRouter([
   {
     path: ROUTER_PATH.MAIN,
     element: <MainPage />,
+  },
+  {
+    path: ROUTER_PATH.SIGNUP,
+    element: <SignupPage />,
+  },
+  {
+    path: ROUTER_PATH.LOGIN,
+    element: <LoginPage />,
+  },
+  {
+    path: ROUTER_PATH.RISK_DIAGNOSTICS,
+    element: <RiskDiagnosticsPage />,
+  },
+  {
+    path: ROUTER_PATH.RELIABILITY_DIAGNOSTICS,
+    element: <ReliabilityDiagnosticsPage />,
   },
 ]);
 
