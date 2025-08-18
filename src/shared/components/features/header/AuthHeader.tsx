ffb9e579283c11002253c893afce08b95e285abf
Dobbymin <gmin990624@gmail.com>
2025-08-19 02:49:57 +0900

    feat: 헤더 컴포넌트에 로고 섹션 및 로그인 버튼 추가

--- src/shared/components/features/header/AuthHeader.tsx
+++ src/shared/components/features/header/AuthHeader.tsx
@@ -1,8 +1,11 @@
 import { HEADER_TOKENS } from '../../../styles';
 import { cn } from '../../../utils';
+import { LogoSection } from '../../common';
 
 export const AuthHeader = () => {
   return (
-    <header className={cn(HEADER_TOKENS.base, HEADER_TOKENS.variants.auth)}>AuthHeader</header>
+    <header className={cn(HEADER_TOKENS.base, HEADER_TOKENS.variants.auth)}>
+      <LogoSection />
+    </header>
   );
 };
