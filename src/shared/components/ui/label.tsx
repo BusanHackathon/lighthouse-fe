fea291f7dfd715c374d6fb8240624d2bb63ef404
Dobbymin <gmin990624@gmail.com>
2025-08-18 19:06:09 +0900

    chore: Label 컴포넌트에서 cn 유틸리티 경로 수정

--- src/shared/components/ui/label.tsx
+++ src/shared/components/ui/label.tsx
@@ -2,8 +2,9 @@
 
 import * as React from 'react';
 
-import { cn } from '@/shared/utils/index';
 import * as LabelPrimitive from '@radix-ui/react-label';
+
+import { cn } from '../../utils';
 
 function Label({ className, ...props }: React.ComponentProps<typeof LabelPrimitive.Root>) {
   return (
