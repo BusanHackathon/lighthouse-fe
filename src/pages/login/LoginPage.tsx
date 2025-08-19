8b5fda74302e8994cce668833283b1d1bac10b73
Dobbymin <gmin990624@gmail.com>
2025-08-20 00:18:18 +0900

    refactor: 로그인 페이지에서 기존 로고 섹션을 AuthLogoSection으로 변경 및 경로 수정

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,10 +1,11 @@
-import { LoginContentsSection, LogoSection } from '@/features';
+import { LoginContentsSection } from '@/features';
+import { AuthLogoSection } from '@/shared';
 
 export default function LoginPage() {
   return (
     <div className='flex h-screen flex-col items-center justify-center'>
       <div className='flex flex-col items-center gap-12'>
-        <LogoSection />
+        <AuthLogoSection />
         <LoginContentsSection />
       </div>
     </div>
