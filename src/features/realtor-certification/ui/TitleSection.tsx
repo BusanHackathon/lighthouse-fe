94d70c5d4c284cecac46b658a41f99f71f75785f
Dobbymin <gmin990624@gmail.com>
2025-08-20 02:37:36 +0900

    feat: 공인중개사 인증 섹션 제목 컴포넌트 추가

---
+++ src/features/realtor-certification/ui/TitleSection.tsx
@@ -0,0 +1,8 @@
+export const TitleSection = () => {
+  return (
+    <h1 className='flex flex-col items-center text-[28px] font-semibold'>
+      <span>공인중개사 인증하고</span>
+      <span>간편하고 투명하게 계약해요!</span>
+    </h1>
+  );
+};
