4643fe93e1d39de774b2295da7368c1e9bd32eb5
Dobbymin <gmin990624@gmail.com>
2025-08-19 11:19:43 +0900

    feat: 버튼 컴포넌트 색상 및 스타일 업데이트

--- src/shared/components/ui/button.tsx
+++ src/shared/components/ui/button.tsx
@@ -10,18 +10,19 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground shadow-xs hover:bg-primary/90',
+        default: 'bg-lighthouse-blue text-white shadow-xs rounded-full hover:bg-lighthouse-blue/90',
         destructive:
           'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
         outline:
           'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
-        secondary: 'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
+        secondary:
+          'bg-lighthouse-button-secondary rounded-xl text-lighthouse-button-secondary-text shadow-xs hover:bg-lighthouse-button-secondary-hover active:bg-lighthouse-button-secondary-hover hover:text-lighthouse-blue active:text-lighthouse-blue',
         ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
-        sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
+        sm: 'h-7 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
         lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
         icon: 'size-9',
       },
