2c3b3458dc171c7790132a5f5001119c13336521
Dobbymin <gmin990624@gmail.com>
2025-08-19 22:51:44 +0900

    feat: 카카오 및 네이버 로그인 버튼 컴포넌트 추가

---
+++ src/features/login/components/common/buttons/KakaoLoginButton.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/shared';
+
+import KakaoSymbol from '../../../_assets/kakao-symbol.webp';
+
+export const KakaoLoginButton = () => {
+  return (
+    <Button className='flex h-11 w-90 items-center gap-3 rounded-lg bg-kakao text-black hover:bg-kakao/80 active:bg-kakao/80'>
+      <img src={KakaoSymbol} className='size-4' alt='kakao' />
+      <span className='text-sm font-semibold'>카카오 계정으로 계속하기</span>
+    </Button>
+  );
+};
