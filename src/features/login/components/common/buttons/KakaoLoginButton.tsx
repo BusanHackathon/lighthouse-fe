b11012df68a56baea08759c9e22af5ee7bacbf10
Dobbymin <gmin990624@gmail.com>
2025-08-20 00:42:27 +0900

    feat: 카카오 및 네이버 로그인 버튼의 이미지 크기 속성 추가

--- src/features/login/components/common/buttons/KakaoLoginButton.tsx
+++ src/features/login/components/common/buttons/KakaoLoginButton.tsx
@@ -5,7 +5,7 @@ import KakaoSymbol from '../../../_assets/kakao-symbol.webp';
 export const KakaoLoginButton = () => {
   return (
     <Button className='flex h-11 w-90 items-center gap-3 rounded-lg bg-kakao text-black hover:bg-kakao/80 active:bg-kakao/80'>
-      <img src={KakaoSymbol} className='size-4' alt='kakao' />
+      <img src={KakaoSymbol} className='size-4' width={16} height={16} alt='kakao' />
       <span className='text-sm font-semibold'>카카오 계정으로 계속하기</span>
     </Button>
   );
--- src/features/login/components/common/buttons/NaverLoginButton.tsx
+++ src/features/login/components/common/buttons/NaverLoginButton.tsx
@@ -5,7 +5,7 @@ import NaverSymbol from '../../../_assets/naver-symbol.webp';
 export const NaverLoginButton = () => {
   return (
     <Button className='flex h-11 w-90 items-center gap-3 rounded-lg bg-naver text-white hover:bg-naver/90 active:bg-naver/80'>
-      <img src={NaverSymbol} className='size-4' alt='naver' />
+      <img src={NaverSymbol} className='size-4' width={16} height={16} alt='naver' />
       <span className='text-sm font-semibold'>네이버 계정으로 계속하기</span>
     </Button>
   );
