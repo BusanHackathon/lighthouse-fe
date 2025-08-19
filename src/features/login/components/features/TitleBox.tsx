0f1b35da7d5b1df87a4b469548c115915a67e5d3
Dobbymin <gmin990624@gmail.com>
2025-08-19 23:30:06 +0900

    feat: 제목 박스 컴포넌트의 텍스트 구조 개선 및 스타일 수정

--- src/features/login/components/features/TitleBox.tsx
+++ src/features/login/components/features/TitleBox.tsx
@@ -1,11 +1,13 @@
 export const TitleBox = () => {
   return (
     <div className='flex flex-col items-center gap-3'>
-      <div className='flex flex-col items-center'>
-        <h1 className='text-[28px] font-semibold'>간편 가입하고</h1>
-        <h1 className='text-[28px] font-semibold'>안전한 내집 마련 하세요</h1>
-      </div>
-      <span className='text-md font-medium text-[#8A8A8A]'>전세사기를 막는 가장 쉬운 방법</span>
+      <h1 className='flex flex-col items-center text-[28px] font-semibold'>
+        <span>간편 가입하고</span>
+        <span>안전한 내집 마련 하세요</span>
+      </h1>
+      <span className='text-md font-medium text-input-placeholder-gray'>
+        전세사기를 막는 가장 쉬운 방법
+      </span>
     </div>
   );
 };
