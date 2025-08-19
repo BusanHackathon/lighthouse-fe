a9d31c8bf36b89a3f34f0befb520f197315799c7
Dobbymin <gmin990624@gmail.com>
2025-08-20 00:33:00 +0900

    refactor: App에서 ImagePreloader를 제거하고 ApplicationProviders에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { ApplicationProviders } from './app/providers';
 import { Routes } from './app/routes';
-import { ImagePreloader } from './shared';
 
 function App() {
   return (
     <ApplicationProviders>
-      <ImagePreloader />
       <Routes />
     </ApplicationProviders>
   );
