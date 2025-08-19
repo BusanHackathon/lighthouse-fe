7cf3ff7e41ba5c32b7718d057a432b534f9e34fe
Dobbymin <gmin990624@gmail.com>
2025-08-19 10:49:59 +0900

    feat: 아바타 컴포넌트 추가

--- src/shared/components/ui/index.ts
+++ src/shared/components/ui/index.ts
@@ -1,3 +1,7 @@
 export * from './button';
 export * from './checkbox';
+export * from './input';
 export * from './sonner';
+export * from './form';
+export * from './label';
+export * from './avatar';
