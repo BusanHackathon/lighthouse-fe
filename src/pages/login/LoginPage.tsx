70d8631764c23dfbd5ea928e6a3ede22f48b7a93
Dobbymin <gmin990624@gmail.com>
2025-08-19 23:22:41 +0900

    feat: 로그인 페이지 레이아웃 개선 및 로고와 로그인 내용 섹션 통합

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,3 +1,12 @@
+import { LoginContentsSection, LogoSection } from '@/features';
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div className='flex h-screen flex-col items-center justify-center'>
+      <div className='flex flex-col items-center gap-12'>
+        <LogoSection />
+        <LoginContentsSection />
+      </div>
+    </div>
+  );
 }
