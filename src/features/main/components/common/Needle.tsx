0908db3deda752c778207fc6db39e3a223ad9e25
Dobbymin <gmin990624@gmail.com>
2025-08-21 21:04:15 +0900

    refactor: Needle 컴포넌트의 크기 및 위치 조정으로 반응형 디자인 개선

--- src/features/main/components/common/Needle.tsx
+++ src/features/main/components/common/Needle.tsx
@@ -7,14 +7,14 @@ type Props = {
 export const Needle = ({ gaugeData }: Props) => {
   return (
     <div
-      className='absolute top-1/2 left-1/2 h-24 origin-bottom -translate-x-1/2 -translate-y-full transition-transform duration-500 ease-in-out'
+      className='absolute top-1/2 left-1/2 origin-bottom -translate-x-1/2 -translate-y-full transition-transform duration-500 ease-in-out sm:h-20 md:h-16 lg:h-24'
       style={{
         transform: `rotate(${gaugeData.angle}deg)`,
       }}
     >
-      <div className='absolute bottom-0 left-1/2 z-10 h-6 w-6 -translate-x-1/2 translate-y-1/2 rounded-full bg-gray-400' />
+      <div className='absolute bottom-0 left-1/2 z-10 size-2 -translate-x-1/2 translate-y-1/2 rounded-full bg-gray-400 sm:size-4 md:size-5 lg:size-6' />
 
-      <NeedleIcon className='absolute bottom-0 left-1/2 h-24 w-50 -translate-x-1/2 rounded-md text-gray-800' />
+      <NeedleIcon className='absolute bottom-0 left-1/2 h-12 w-24 -translate-x-1/2 rounded-md text-gray-800 sm:h-16 sm:w-32 md:h-20 md:w-40 lg:h-24 lg:w-50' />
     </div>
   );
 };
