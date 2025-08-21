637cdf3faa2320595ebbcf2ddb0abd9ddf88a46d
Dobbymin <gmin990624@gmail.com>
2025-08-21 13:41:31 +0900

    fix: 메인 페이지의 레이아웃을 수정하여 최소 높이를 설정

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -2,7 +2,7 @@ import { ChartSection, InputSection, MapSection } from '@/features';
 
 export default function MainPage() {
   return (
-    <div className='flex h-screen w-full flex-col'>
+    <div className='flex min-h-screen w-full flex-col'>
       <div className='flex w-full gap-4'>
         <InputSection />
         <MapSection />
