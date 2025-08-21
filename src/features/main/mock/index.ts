af564b1f19a6577234258333366132fbe14efaee
Dobbymin <gmin990624@gmail.com>
2025-08-22 03:22:41 +0900

    feat: 매물 데이터 추가

--- src/features/main/mock/index.ts
+++ src/features/main/mock/index.ts
@@ -1,3 +1,4 @@
 export * from './chart-data.mock';
-export * from './risk-analysis.mock';
 export * from './landlord-reliability.mock';
+export * from './property-data.mock';
+export * from './risk-analysis.mock';
---
+++ src/features/main/mock/property-data.mock.ts
@@ -0,0 +1,46 @@
+import type { Property } from '../types';
+
+export const SERVER_PROPERTIES: Property[] = [
+  {
+    id: 1,
+    type: '오피스텔',
+    buildingName: '센텀시티 파크빌',
+    address: '부산 해운대구 센텀시티 대로 123',
+  },
+  {
+    id: 2,
+    type: '원룸',
+    buildingName: '센텀시티 파크빌',
+    address: '부산 해운대구 센텀시티 대로 123',
+  },
+  {
+    id: 3,
+    type: '투룸',
+    buildingName: '센텀시티 파크빌',
+    address: '부산 해운대구 센텀시티 대로 123',
+  },
+  {
+    id: 4,
+    type: '아파트',
+    buildingName: '센텀시티 파크빌',
+    address: '부산 해운대구 센텀시티 대로 123',
+  },
+  {
+    id: 5,
+    type: '복층',
+    buildingName: '센텀시티 파크빌',
+    address: '부산 해운대구 센텀시티 대로 123',
+  },
+  {
+    id: 6,
+    type: '빌라',
+    buildingName: '센텀시티 파크빌',
+    address: '부산 해운대구 센텀시티 대로 123',
+  },
+  {
+    id: 7,
+    type: '단독주택',
+    buildingName: '센텀시티 파크빌',
+    address: '부산 해운대구 센텀시티 대로 123',
+  },
+];
