65b97d1dbc7765bfb6ee70c53b9987e4bde5d603
Dobbymin <gmin990624@gmail.com>
2025-08-21 13:27:43 +0900

    feat: 차트 섹션 컴포넌트 추가

---
+++ src/features/main/ui/ChartSection.tsx
@@ -0,0 +1,16 @@
+import { ChartBox } from '../components';
+import { CHART_DATA_CONFIGS } from '../mock';
+
+export const ChartSection = () => {
+  return (
+    <div className='mx-auto w-full max-w-6xl p-6'>
+      <div className='grid grid-cols-2 gap-6'>
+        {CHART_DATA_CONFIGS.map((config, index) => (
+          <div key={index} className='bg-white'>
+            <ChartBox data={config.data} lines={config.lines} title={config.title} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
