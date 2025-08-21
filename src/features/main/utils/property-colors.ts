24480f89747824ce6869f92e41a06d596b248f0f
Dobbymin <gmin990624@gmail.com>
2025-08-22 03:40:23 +0900

    refactor: 매물 타입 색상 처리 방식을 동적 해시 기반으로 변경

--- src/features/main/utils/property-colors.ts
+++ src/features/main/utils/property-colors.ts
@@ -1,31 +1,23 @@
 import { FALLBACK_COLORS, PROPERTY_TYPE_COLORS } from '../constants';
 
-// 동적 색상 생성 함수 (정의되지 않은 타입용)
-const generateDynamicColors = () => {
-  let colorIndex = 0;
-
-  return (type: string): string => {
-    // 이미 정의된 타입이면 해당 색상 반환
-    if (type in PROPERTY_TYPE_COLORS) {
-      return PROPERTY_TYPE_COLORS[type as keyof typeof PROPERTY_TYPE_COLORS];
-    }
-
-    // 정의되지 않은 타입이면 동적 색상 할당
-    const color = FALLBACK_COLORS[colorIndex % FALLBACK_COLORS.length];
-    colorIndex++;
-    return color;
-  };
-};
-
-const getColorForType = generateDynamicColors();
-
 /**
  * 매물 타입에 따른 색상을 반환하는 함수
  * @param type - 매물 타입
  * @returns 색상 코드
  */
 export const getPropertyTypeColor = (type: string): string => {
-  return getColorForType(type);
+  // 이미 정의된 타입이면 해당 색상 반환
+  if (type in PROPERTY_TYPE_COLORS) {
+    return PROPERTY_TYPE_COLORS[type as keyof typeof PROPERTY_TYPE_COLORS];
+  }
+
+  // 정의되지 않은 타입이면 해시를 기반으로 동적 색상 할당
+  let hash = 0;
+  for (let i = 0; i < type.length; i++) {
+    hash = type.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  const index = Math.abs(hash) % FALLBACK_COLORS.length;
+  return FALLBACK_COLORS[index];
 };
 
 /**
@@ -36,6 +28,6 @@ export const getPropertyTypeColor = (type: string): string => {
 export const addColorsToProperties = <T extends { type: string }>(properties: T[]) => {
   return properties.map((property) => ({
     ...property,
-    typeColor: getColorForType(property.type),
+    typeColor: getPropertyTypeColor(property.type),
   }));
 };
