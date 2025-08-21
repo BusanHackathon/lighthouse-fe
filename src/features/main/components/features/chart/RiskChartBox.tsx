d186abe0f9900006c3311cca7805a5e5c97261b0
Dobbymin <gmin990624@gmail.com>
2025-08-21 20:06:09 +0900

    style: 위험도 차트 박스 컴포넌트의 텍스트 크기 및 레이아웃 수정, 게이지 바늘 위치 조정

--- src/features/main/components/features/chart/RiskChartBox.tsx
+++ src/features/main/components/features/chart/RiskChartBox.tsx
@@ -4,8 +4,6 @@ import { type GaugeData, getGaugeData } from '@/shared';
 
 import { Needle } from '../../common';
 
-// import NeedleIcon from '../../../_assets/needle.svg';
-
 type Props = {
   riskScore: number;
 };
@@ -14,15 +12,15 @@ export const RiskChartBox = ({ riskScore }: Props) => {
   const gaugeData: GaugeData = getGaugeData(riskScore);
 
   return (
-    <div className='w-full rounded-lg bg-white p-6 shadow-sm'>
-      <h3 className='mb-2 text-lg font-bold text-gray-900'>전세 계약 최종 위험도</h3>
+    <div className='w-full rounded-lg bg-white p-6'>
+      <h3 className='mb-2 text-2xl font-bold text-gray-900'>전세 계약 최종 위험도</h3>
       <p className='text-md mb-6 flex items-center gap-2 text-gray-600'>
         <span className='font-semibold'>위험 점수 :</span>
         <span className='font-semibold text-green-600'>{riskScore}</span>
         <span className='text-gray-600'>점</span>
       </p>
 
-      <div className='relative h-64 w-full'>
+      <div className='relative h-90 w-full'>
         <ResponsiveContainer width='100%' height='100%'>
           <PieChart>
             <Pie
@@ -31,8 +29,8 @@ export const RiskChartBox = ({ riskScore }: Props) => {
               cy='50%'
               startAngle={0}
               endAngle={180}
-              innerRadius={65}
-              outerRadius={100}
+              innerRadius={130}
+              outerRadius={180}
               paddingAngle={4}
               cornerRadius={6}
               dataKey='value'
@@ -41,12 +39,12 @@ export const RiskChartBox = ({ riskScore }: Props) => {
           </PieChart>
         </ResponsiveContainer>
 
-        {/* --- 게이지 바늘 --- */}
+        {/*  게이지 바늘  */}
         <Needle gaugeData={gaugeData} />
 
         {/* 위험도 레이블 */}
         <div
-          className='absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-8 transform rounded-full px-4 py-2 text-sm font-medium text-white shadow-sm'
+          className='absolute top-1/2 left-1/2 -translate-x-1/2 translate-y-16 transform rounded-full px-4 py-2 text-sm font-medium text-white shadow-sm'
           style={{ backgroundColor: gaugeData.color }}
         >
           {gaugeData.label}
