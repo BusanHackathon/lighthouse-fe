95738b0771631a7cf1fec6adae70e4f0742e84e8
Dobbymin <gmin990624@gmail.com>
2025-08-22 06:16:02 +0900

    fix: BASE_URL을 로컬호스트에서 실제 서버 IP로 변경

--- src/shared/libs/fetch-instance.ts
+++ src/shared/libs/fetch-instance.ts
@@ -1,10 +1,9 @@
 import { initInstance } from './axios-instance';
 
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'http://43.200.101.253';
 
 export const fetchInstance = initInstance({
   baseURL: BASE_URL,
-  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
