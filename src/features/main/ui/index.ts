07647652a2919757358ae8e210cdba1c46116987
Dobbymin <gmin990624@gmail.com>
2025-08-22 02:13:27 +0900

    feat: 임대인 신뢰도 섹션 컴포넌트 추가 및 인덱스 파일에 내보내기 추가

--- src/features/main/ui/index.ts
+++ src/features/main/ui/index.ts
@@ -2,3 +2,4 @@ export * from './ChartSection';
 export * from './InputSection';
 export * from './MapSection';
 export * from './RiskAnalysisSummarySection';
+export * from './LandlordReliabilitySection';
