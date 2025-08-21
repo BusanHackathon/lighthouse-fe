302935b2889a7c43d1ee58cf599a4ebccdfc7f9e
Dobbymin <gmin990624@gmail.com>
2025-08-21 13:27:55 +0900

    feat: 메인 페이지에 차트 섹션 추가 및 레이아웃 수정

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,10 +1,15 @@
-import { InputSection, MapSection } from '@/features';
+import { ChartSection, InputSection, MapSection } from '@/features';
 
 export default function MainPage() {
   return (
-    <div className='flex h-screen w-full'>
-      <InputSection />
-      <MapSection />
+    <div className='flex h-screen w-full flex-col'>
+      <div className='flex w-full gap-4'>
+        <InputSection />
+        <MapSection />
+      </div>
+      <div className='flex w-full gap-2'>
+        <ChartSection />
+      </div>
     </div>
   );
 }
