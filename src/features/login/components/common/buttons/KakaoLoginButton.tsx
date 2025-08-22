d927d1c90cc02fcfb3d476f400d9a38c21a64337
Dobbymin <gmin990624@gmail.com>
2025-08-23 01:24:21 +0900

    feat: 카카오 로그인 버튼 기능 수정 및 API 경로 통합

--- src/features/login/components/common/buttons/KakaoLoginButton.tsx
+++ src/features/login/components/common/buttons/KakaoLoginButton.tsx
@@ -1,19 +1,19 @@
-import { useKakaoLogin } from '@/entities';
-import { Button } from '@/shared';
+import { BASE_URL, Button } from '@/shared';
 
 import KakaoSymbol from '../../../_assets/kakao-symbol.webp';
 
 export const KakaoLoginButton = () => {
-  const { mutate: startKakaoLogin, isPending } = useKakaoLogin();
+  const startKakaoLogin = () => {
+    window.location.href = `${BASE_URL}/oauth2/authorization/kakao`;
+  };
+
   return (
     <Button
       onClick={() => startKakaoLogin()}
       className='flex h-11 w-90 items-center gap-3 rounded-lg bg-kakao text-black hover:bg-kakao/80 active:bg-kakao/80'
     >
       <img src={KakaoSymbol} className='size-4' width={16} height={16} alt='kakao' />
-      <span className='text-sm font-semibold'>
-        {isPending ? '이동중' : '카카오 계정으로 계속하기'}
-      </span>
+      <span className='text-sm font-semibold'>카카오 계정으로 계속하기</span>
     </Button>
   );
 };
