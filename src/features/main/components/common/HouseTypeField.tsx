0635573580c42222c6155084f2eaba9bb33be8a8
Dobbymin <gmin990624@gmail.com>
2025-08-22 14:06:01 +0900

    feat: 주택 유형 필드에 Select 컴포넌트 적용 및 옵션 표시 기능 추가

--- src/features/main/components/common/HouseTypeField.tsx
+++ src/features/main/components/common/HouseTypeField.tsx
@@ -1,20 +1,50 @@
 import { useFormContext } from 'react-hook-form';
 
-import { FormField, FormItem, FormLabel, FormMessage, Input } from '@/shared';
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/shared';
 
-import { FORM_FIELDS, LABEL_TEXTS, PLACEHOLDER_TEXTS } from '../../constants';
+import { HOUSE_TYPE_OPTIONS, LABEL_TEXTS } from '../../constants';
 import type { SearchAddressType } from '../../model';
 
 export const HouseTypeField = () => {
   const form = useFormContext<SearchAddressType>();
+
   return (
     <FormField
       control={form.control}
       name='houseType'
       render={({ field }) => (
         <FormItem className='flex flex-col gap-2'>
-          <FormLabel htmlFor={FORM_FIELDS.HOUSE_TYPE}>{LABEL_TEXTS.HOUSE_TYPE}</FormLabel>
-          <Input {...field} placeholder={PLACEHOLDER_TEXTS.HOUSE_TYPE} />
+          <FormLabel>{LABEL_TEXTS.HOUSE_TYPE}</FormLabel>
+          <Select onValueChange={field.onChange} defaultValue={field.value}>
+            <FormControl>
+              <SelectTrigger className='w-full rounded-full border-none shadow-[0px_4px_30px_0px_#0000001A] focus-visible:border-lighthouse-blue focus-visible:shadow-lighthouse-blue-shadow [&_span]:text-input-placeholder-gray'>
+                <SelectValue placeholder='주택유형을 선택해주세요' />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>{LABEL_TEXTS.HOUSE_TYPE}</SelectLabel>
+                {HOUSE_TYPE_OPTIONS.map((option) => (
+                  <SelectItem key={option.value} value={option.value}>
+                    {option.label}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
           <FormMessage />
         </FormItem>
       )}
