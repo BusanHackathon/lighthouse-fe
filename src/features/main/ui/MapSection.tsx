c6e62ed3941c55885478cc17584738f623348b73
Dobbymin <gmin990624@gmail.com>
2025-08-22 18:59:14 +0900

    feat: 지도 컴포넌트에서 주소 상태 관리 훅을 사용하여 위도 및 경도 동적 설정

--- src/features/main/ui/MapSection.tsx
+++ src/features/main/ui/MapSection.tsx
@@ -1,8 +1,15 @@
-import { Button, KakaoMap } from '@/shared';
+import { KakaoMap } from '@/entities';
+import { BEXCO_ADDRESS, Button } from '@/shared';
 
 import { HOUSE_TYPES } from '../constants';
+import { useMapAddress } from '../store';
 
 export const MapSection = () => {
+  const { mapAddress } = useMapAddress();
+
+  const lat = mapAddress?.latitude || BEXCO_ADDRESS.latitude;
+  const lng = mapAddress?.longitude || BEXCO_ADDRESS.longitude;
+
   return (
     <div className='flex w-1/2 flex-col border-l border-gray-200'>
       <div className='flex gap-3 border-b border-gray-200 p-4'>
@@ -13,9 +20,8 @@ export const MapSection = () => {
         ))}
       </div>
 
-      {/* TODO: 추후 지도 (카카오맵, 네이버 지도, 구글 맵 등) 컴포넌트 추가 */}
       <div className='flex flex-1 items-center justify-center bg-gray-100'>
-        <KakaoMap lat={35.1690637154991} lng={129.136018268316} />
+        <KakaoMap lat={lat} lng={lng} />
       </div>
     </div>
   );
