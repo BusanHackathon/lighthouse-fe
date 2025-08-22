03d1e80f5b0e8e57084fb573a4c3ed0566cabeaa
Dobbymin <gmin990624@gmail.com>
2025-08-22 21:53:07 +0900

    feat: SubrogationPaymentBox 컴포넌트에 subrogationData prop 추가 및 데이터 처리 로직 수정

--- src/features/main/components/features/reliability/SubrogationPaymentBox.tsx
+++ src/features/main/components/features/reliability/SubrogationPaymentBox.tsx
@@ -2,15 +2,19 @@ import { getSubrogationColorClass } from '@/shared';
 
 import { LANDLORD_TRUST_DATA } from '../../../mock';
 
-export const SubrogationPaymentBox = () => {
+type Props = {
+  subrogationData: number;
+};
+
+export const SubrogationPaymentBox = ({ subrogationData }: Props) => {
+  const data = subrogationData || LANDLORD_TRUST_DATA.subrogationCount;
+
   return (
     <div>
       <h1 className='text-3xl font-bold'>대위변제 이력</h1>
       <div className='flex w-full items-end justify-center'>
-        <span
-          className={`text-landlord-reliability font-bold ${getSubrogationColorClass(LANDLORD_TRUST_DATA.subrogationCount)}`}
-        >
-          {LANDLORD_TRUST_DATA.subrogationCount}
+        <span className={`text-landlord-reliability font-bold ${getSubrogationColorClass(data)}`}>
+          {data}
         </span>
         <span className='mb-25 text-5xl font-semibold text-black'>회</span>
       </div>
