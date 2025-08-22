d530ceb2b6b8afc24c4006964b74882dbf3b221d
Dobbymin <gmin990624@gmail.com>
2025-08-22 15:56:13 +0900

    feat: 주소 검색 훅(useAddressSearch) 구현

--- src/features/main/hooks/index.ts
+++ src/features/main/hooks/index.ts
@@ -1,1 +1,2 @@
 export * from './useChartConfig';
+export * from './useAddressSearch';
---
+++ src/features/main/hooks/useAddressSearch.ts
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import { useFormContext } from 'react-hook-form';
+
+import type { SearchAddressType } from '../model';
+import type { AddressType } from '../types';
+
+export const useAddressSearch = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const form = useFormContext<SearchAddressType>();
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape' && isOpen) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('keydown', handleKeyDown);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [isOpen]);
+
+  const completeAddress = (data: AddressType) => {
+    let fullAddress = data.address;
+    let extraAddress = '';
+
+    if (data.addressType === 'R') {
+      if (data.bname !== '') {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== '') {
+        extraAddress += extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== '' ? ` (${extraAddress})` : '';
+    }
+
+    form.setValue('address', fullAddress);
+    setIsOpen(false);
+  };
+
+  const openSearch = () => {
+    setIsOpen(true);
+  };
+
+  const closeSearch = () => {
+    setIsOpen(false);
+  };
+
+  return {
+    isOpen,
+    completeAddress,
+    openSearch,
+    closeSearch,
+  };
+};
