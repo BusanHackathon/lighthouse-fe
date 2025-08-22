ca52d8efbd0f464d072cd77a8360f5733a725033
Dobbymin <gmin990624@gmail.com>
2025-08-23 02:03:46 +0900

    feat: substitute-compensations.api.ts 파일 추가하여 대체 보상 데이터 API 구현

---
+++ src/entities/chart/apis/substitute-compensations.api.ts
@@ -0,0 +1,19 @@
+import { fetchInstance } from '@/shared';
+
+export const SUBSTITUTE_COMPENSATIONS_API_PATH = '/graph/substituteCompensations';
+
+interface SubstituteCompensationsApiResponse {
+  month: string;
+  cases: number;
+  momchange: number;
+}
+
+export const substituteCompensationsApi = async (): Promise<
+  SubstituteCompensationsApiResponse[]
+> => {
+  const response = await fetchInstance.get<SubstituteCompensationsApiResponse[]>(
+    SUBSTITUTE_COMPENSATIONS_API_PATH,
+  );
+
+  return response.data;
+};
