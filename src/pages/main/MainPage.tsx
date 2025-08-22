983d4ecaf77f6f3b63c17574c3aec0268a6d1277
Dobbymin <gmin990624@gmail.com>
2025-08-22 19:01:40 +0900

    fix: MainPage에서 LandlordPropertySection의 위치 변경

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -17,8 +17,8 @@ export default function MainPage() {
       </div>
       <RiskAnalysisSummarySection />
       <ChartSection />
-      <LandlordPropertySection />
       <LandlordReliabilitySection />
+      <LandlordPropertySection />
       <AlternativeSection />
     </div>
   );
