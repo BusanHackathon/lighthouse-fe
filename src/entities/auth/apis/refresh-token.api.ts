cf07863a12d4e685fcb9065df60eef10811e75bf
Dobbymin <gmin990624@gmail.com>
2025-08-23 08:42:15 +0900

    refactor: REFRESH_TOKEN_API_PATH를 함수로 변경하여 API 경로 동적 생성

--- src/entities/auth/apis/refresh-token.api.ts
+++ src/entities/auth/apis/refresh-token.api.ts
@@ -1,19 +1,20 @@
 import { fetchInstance } from '@/shared';
 
-export const REFRESH_TOKEN_API_PATH = '/api/auth/refresh';
+export const REFRESH_TOKEN_API_PATH = () => '/api/auth/refresh';
 
 interface RefreshTokenApiRequest {
   refreshToken: string;
 }
 
 interface RefreshTokenApiResponse {
+  message: string;
   newAccessToken: string;
 }
 
 export const refreshTokenApi = async ({
   refreshToken,
 }: RefreshTokenApiRequest): Promise<RefreshTokenApiResponse> => {
-  const response = await fetchInstance.post<RefreshTokenApiResponse>(REFRESH_TOKEN_API_PATH, {
+  const response = await fetchInstance.post<RefreshTokenApiResponse>(REFRESH_TOKEN_API_PATH(), {
     params: {
       refreshToken,
     },
