dfdf6d44c608442b4c81edfd5e3e463d1125f8d8
Dobbymin <gmin990624@gmail.com>
2025-08-23 01:21:52 +0900

    feat: refresh-token.api.ts 파일 추가하여 인증 토큰 갱신 API 구현

---
+++ src/entities/auth/apis/refresh-token.api.ts
@@ -0,0 +1,21 @@
+import { fetchInstance } from '@/shared';
+
+export const REFRESH_TOKEN_API_PATH = '/auth/refresh';
+
+interface RefreshTokenApiRequest {
+  refreshToken: string;
+}
+
+interface RefreshTokenApiResponse {
+  accessToken: string;
+}
+
+export const refreshTokenApi = async ({
+  refreshToken,
+}: RefreshTokenApiRequest): Promise<RefreshTokenApiResponse> => {
+  const response = await fetchInstance.post<RefreshTokenApiResponse>(REFRESH_TOKEN_API_PATH, {
+    refreshToken,
+  });
+
+  return response.data;
+};
