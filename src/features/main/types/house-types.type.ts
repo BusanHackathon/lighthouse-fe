3a18633926bbf1628b068dca382ae441d228477a
Dobbymin <gmin990624@gmail.com>
2025-08-22 13:09:28 +0900

    refactor: RENT_TYPES 상수 제거 및 관련 타입 정리

--- src/features/main/types/house-types.type.ts
+++ src/features/main/types/house-types.type.ts
@@ -1,5 +1,4 @@
-import type { FORM_FIELDS, HOUSE_TYPES, RENT_TYPES } from '../constants';
+import type { FORM_FIELDS, HOUSE_TYPES } from '../constants';
 
-export type RentType = (typeof RENT_TYPES)[keyof typeof RENT_TYPES];
 export type HouseType = (typeof HOUSE_TYPES)[number];
 export type FormField = (typeof FORM_FIELDS)[keyof typeof FORM_FIELDS];
