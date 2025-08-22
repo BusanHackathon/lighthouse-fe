3d77d24fbfff022e25c98949bf3ef4881b0ccf47
Dobbymin <gmin990624@gmail.com>
2025-08-23 07:24:04 +0900

    feat: BASE_URL을 로컬호스트에서 실제 서버 주소로 변경하여 API 호출 경로 수정

--- src/shared/libs/fetch-instance.ts
+++ src/shared/libs/fetch-instance.ts
@@ -5,7 +5,6 @@ import { authStorage } from '../utils';
 import { initInstance } from './axios-instance';
 
 export const BASE_URL = 'http://43.200.101.253';
-// export const BASE_URL = 'http://localhost:8080';
 
 export const fetchInstance = initInstance({
   baseURL: BASE_URL,
