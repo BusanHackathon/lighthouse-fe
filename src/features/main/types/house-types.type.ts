763da62b00f26352d93cdb58f94d916174e7dad3
Dobbymin <gmin990624@gmail.com>
2025-08-22 14:43:54 +0900

    feat: 주택 유형 상수에서 HOUSE_TYPES를 HOUSE_TYPE_OPTIONS로 변경 및 HouseType 타입 수정

--- src/features/main/types/house-types.type.ts
+++ src/features/main/types/house-types.type.ts
@@ -1,4 +1,5 @@
-import type { FORM_FIELDS, HOUSE_TYPES } from '../constants';
+import type { FORM_FIELDS, HOUSE_TYPE_OPTIONS } from '../constants';
 
-export type HouseType = (typeof HOUSE_TYPES)[number];
+export type HouseType = (typeof HOUSE_TYPE_OPTIONS)[number]['value'];
+
 export type FormField = (typeof FORM_FIELDS)[keyof typeof FORM_FIELDS];
