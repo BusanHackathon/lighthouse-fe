8c8f326a0e13249104a294e456eb9331359440cf
Dobbymin <gmin990624@gmail.com>
2025-08-22 22:44:46 +0900

    feat: MainPage 컴포넌트에 위험도 분석 섹션으로 스크롤 기능 추가 및 ref 전달 구현

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,3 +1,5 @@
+import { useRef } from 'react';
+
 import {
   AlternativeSection,
   ChartSection,
@@ -9,13 +11,15 @@ import {
 } from '@/features';
 
 export default function MainPage() {
+  const riskAnalysisRef = useRef<HTMLDivElement>(null);
+
   return (
     <div className='flex min-h-screen w-full flex-col'>
       <div className='flex w-full gap-4'>
-        <InputSection />
+        <InputSection scrollToRiskAnalysis={riskAnalysisRef as React.RefObject<HTMLDivElement>} />
         <MapSection />
       </div>
-      <RiskAnalysisSummarySection />
+      <RiskAnalysisSummarySection ref={riskAnalysisRef} />
       <ChartSection />
       <LandlordReliabilitySection />
       <LandlordPropertySection />
