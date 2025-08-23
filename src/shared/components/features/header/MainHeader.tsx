2f0270d87ad801e09bd840dc60b49308ebe631c9
Dobbymin <gmin990624@gmail.com>
2025-08-23 17:44:23 +0900

    feat: 프로필 이미지 추가 및 헤더 컴포넌트에서 로그인 상태에 따라 아바타 또는 로그인 버튼 표시 기능 구현

--- src/shared/components/features/header/MainHeader.tsx
+++ src/shared/components/features/header/MainHeader.tsx
@@ -1,24 +1,33 @@
 import { useNavigate } from 'react-router-dom';
 
+import ProfileImage from '../../../_assets/profile.webp';
 import { ROUTER_PATH } from '../../../constants';
 import { HEADER_TOKENS } from '../../../styles';
-import { cn } from '../../../utils';
+import { authStorage, cn } from '../../../utils';
 import { LogoSection } from '../../common';
-import { Button } from '../../ui';
+import { Avatar, AvatarImage, Button } from '../../ui';
 
 export const MainHeader = () => {
   const navigate = useNavigate();
 
+  const token = authStorage.accessToken.get();
+
   return (
     <header className={cn(HEADER_TOKENS.base, HEADER_TOKENS.variants.main)}>
       <LogoSection />
-      <Button
-        variant='ghost'
-        onClick={() => navigate(ROUTER_PATH.LOGIN)}
-        className='font-semibold text-[#333D48] hover:text-black'
-      >
-        간편 로그인
-      </Button>
+      {token ? (
+        <Avatar>
+          <AvatarImage src={ProfileImage} />
+        </Avatar>
+      ) : (
+        <Button
+          variant='ghost'
+          onClick={() => navigate(ROUTER_PATH.LOGIN)}
+          className='font-semibold text-[#333D48] hover:text-black'
+        >
+          간편 로그인
+        </Button>
+      )}
     </header>
   );
 };
--- src/shared/components/features/header/RealtorHeader.tsx
+++ src/shared/components/features/header/RealtorHeader.tsx
@@ -1,24 +1,33 @@
 import { useNavigate } from 'react-router-dom';
 
+import ProfileImage from '../../../_assets/profile.webp';
 import { ROUTER_PATH } from '../../../constants';
 import { HEADER_TOKENS } from '../../../styles';
-import { cn } from '../../../utils';
+import { authStorage, cn } from '../../../utils';
 import { LogoSection } from '../../common';
-import { Button } from '../../ui';
+import { Avatar, AvatarImage, Button } from '../../ui';
 
 export const RealtorHeader = () => {
   const navigate = useNavigate();
 
+  const token = authStorage.accessToken.get();
+
   return (
     <header className={cn(HEADER_TOKENS.base, HEADER_TOKENS.variants.realtor)}>
       <LogoSection />
-      <Button
-        variant='ghost'
-        className='font-semibold text-[#333D48] hover:text-black'
-        onClick={() => navigate(ROUTER_PATH.REALTOR_LOGIN)}
-      >
-        로그인
-      </Button>
+      {token ? (
+        <Avatar>
+          <AvatarImage src={ProfileImage} />
+        </Avatar>
+      ) : (
+        <Button
+          variant='ghost'
+          className='font-semibold text-[#333D48] hover:text-black'
+          onClick={() => navigate(ROUTER_PATH.REALTOR_LOGIN)}
+        >
+          로그인
+        </Button>
+      )}
     </header>
   );
 };
