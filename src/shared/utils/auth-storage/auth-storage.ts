d844e174cf0bd42bb19c40444a01cb5377f28d3c
Dobbymin <gmin990624@gmail.com>
2025-08-23 17:23:29 +0900

    fix: auth-storage에서 유효하지 않은 값을 저장하려 할 때 경고 메시지를 제거하고, 해당 값을 처리하는 로직을 간소화하여 코드 개선

--- src/shared/utils/auth-storage/auth-storage.ts
+++ src/shared/utils/auth-storage/auth-storage.ts
@@ -13,9 +13,7 @@ const initStorage = <T extends keyof AuthStorageKey>(key: T, storage: Storage) =
   };
 
   const set = (value: AuthStorageKey[T]) => {
-    // undefined, null, 빈 문자열, 'undefined' 문자열 체크
     if (value === undefined || value === null || value === '' || value === 'undefined') {
-      console.warn(`Attempting to store invalid value for ${key}:`, value);
       return storage.removeItem(storageKey);
     }
 
